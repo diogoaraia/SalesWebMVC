fa3f0399e02a8577da7ced8fe88a8b4912c319d9
Diogo Araia <diogoaraia@gmail.com>
2020-08-07 20:17:51 -0300

    Alterado tema do Bootstrap, criado entidades e seus atributos, contrutores, metodos, associado entidades, adicionado dbset's no dbcontext, adicionado nova migration, update database

--- SalesWebMVC/Models/Department.cs
+++ SalesWebMVC/Models/Department.cs
@@ -1,8 +1,38 @@
-﻿namespace SalesWebMVC.Models
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SalesWebMVC.Models
 {
     public class Department
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public ICollection<Seller> Sellers { get; set; } = new List<Seller>();
+                
+        public Department() 
+        {
+        }
+
+        public Department(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+    
+        // Adicionar vendedor
+        public void AddSeller(Seller seller)
+        {
+            Sellers.Add(seller);
+        }
+
+        public double TotalSales(DateTime initial, DateTime final)
+        {
+            // return Sellers = pegar a lista de vendedores
+            // .Sum(apenas as vendas nesse periodo de data)
+            // seller => para cada vendedor
+            // seller.TotalSales(initial, final) pegar o total de vendas no periodo e somar ao de todos vendedores
+            return Sellers.Sum(seller => seller.TotalSales(initial, final));
+        }
     }
 }
---
+++ SalesWebMVC/Models/Seller.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SalesWebMVC.Models
+{
+    public class Seller
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public DateTime BirthDate { get; set; }
+        public double BaseSalary { get; set; }
+        public Department Department { get; set; }
+        public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
+
+        public Seller() 
+        {
+        }
+
+        public Seller(int id, string name, string email, DateTime birthDate, double baseSalary, Department department)
+        {
+            Id = id;
+            Name = name;
+            Email = email;
+            BirthDate = birthDate;
+            BaseSalary = baseSalary;
+            Department = department;
+        }
+
+        // Adicionar vendas de sr em SalesRecord
+        public void AddSales(SalesRecord sr)
+        {
+            Sales.Add(sr);
+        }
+
+        // Remover vendas de sr em SalesRecord
+        public void RemoveSales(SalesRecord sr)
+        {
+            Sales.Remove(sr);
+        }
+
+        // Trazer o total de vendas entre a data inicial e final
+        // Necessário incluir no inicio using System.Linq;
+        public double TotalSales(DateTime initial, DateTime final)
+        {
+            // Sales = lista de vendas associada ao vendedor
+            // Where = filtro do período
+            // sr = SalesRecord
+            // Pegar todo objeto sr, tal que sr.Date >= initial e sr.Date <= final
+            // Após calcular a soma baseado na soma do sr que leva em sr.Amount
+            return Sales.Where(sr => sr.Date >= initial && sr.Date <= final).Sum(sr => sr.Amount);
+        }
+    }
+}
